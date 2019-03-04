a1f966533ef3fcea80f999eb2b6a9481664e57ce
hansama <hanshanglin2017@gmail.com>
2019-03-05 01:28:13 +0800

    add more interface about date and time

--- mytime.h
+++ mytime.h
@@ -1,32 +1,32 @@
 #ifndef MYTIME_H_INCLUDED
 #define MYTIME_H_INCLUDED
 
-//priority Project > Assignment > Revision > Activity
-enum task_type
-{
-    Project,
-    Assignment,
-    Revision,
-    Activity
-};
 
 typedef struct
 {
     int days_since_base; //the day since the base day
-    int time_slot; //if not ,-1
+    int time_slot;       //if not ,-1
 } Date;
 
-typedef struct
-{
-    task_type type; // enum type
-    char *id; // subjest code
-    Date day; // struct Date
-    int duration; // number of time slot, need to range
-} Record;
 
-char *start_date; // the period start date "YYYY-MM-DD"
-char *end_date; // the period end date "YYYY-MM-DD"
-int start_time; // the period start time "hh" (0~24)
-int end_time; // the period end time "hh" (0~24)
+//check valid date "YYYY-MM-DD"
+bool is_valid_date(char* date)
+
+// "YYYY-MM-DD" to store at globol char *end_date 
+bool set_end_date(char* date)
+
+// "YYYY-MM-DD" to store at globol char *start_date 
+bool set_start_date(char* date)
+
+//get date_to_base
+int convert_to_base(char* date)
+
+//get date_to_realdate
+char* convert_to_date(int num)
+
+static char *start_date; // the period start date "YYYY-MM-DD"
+static char *end_date;   // the period end date "YYYY-MM-DD"
+static int start_time;   // the period start time "hh" (0~24)
+static int end_time;     // the period end time "hh" (0~24)
 
 #endif
--- time.c
+++ time.c
@@ -2,7 +2,52 @@
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <assert.h>
 #include "mytime.h"
 
+// "YYYY-MM-DD" to store at globol char *start_date 
+bool set_start_date(char* date){
+    assert(is_valid_date(date));
 
+    start_date = date;
+    return true;
+}
+
+// "YYYY-MM-DD" to store at globol char *end_date 
+bool set_end_date(char* date){
+    assert(is_valid_date(date));
+
+    start_date = date;
+    return true;
+}
+
+//get date_to_base
+int convert_to_base(char* date){
+    assert(is_valid_date(date));
+
+    //todomark
+    return 0;
+}
+
+//get date_to_realdate
+char* convert_to_date(int num){
+
+    //todomark
+    return NULL;
+}
+
+
+//check valid date "YYYY-MM-DD"
+bool is_valid_date(char* date){
+        if(strlen(date) != 10)
+        return false;
+    for(size_t i = 0; i < strlen(date); i++)
+    {   
+        if(i == 4 || i == 7)// '-'
+            continue;
+        if('9'<date[i] || '0'>date[i])
+            return false;
+    }
+    return true;
+}
