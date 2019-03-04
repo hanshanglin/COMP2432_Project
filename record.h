33093e9f215f9a39f504371b9473061789ae4222
hansama <hanshanglin2017@gmail.com>
2019-03-05 01:28:47 +0800

    adjust some struct from mytime.h

--- record.h
+++ record.h
@@ -9,6 +9,24 @@ typedef struct
     Data_record* next;
 }Data_record;
 
+//priority Project > Assignment > Revision > Activity
+enum task_type
+{
+    Project,
+    Assignment,
+    Revision,
+    Activity
+};
+
+typedef struct
+{
+    task_type type; // enum type
+    char *id;       // subjest code
+    Date day;       // struct Date
+    int duration;   // number of time slot, need to range
+} Record;
+
+
 void add_data(Data_record* block, Record record)
 
 Record next(Data_record* block)
