241630463e31df1ea7ae94d90cf2b9d322d9b212
hansama <hanshanglin2017@gmail.com>
2019-03-05 00:50:00 +0800

    add more document

--- mytime.h
+++ mytime.h
@@ -1,21 +1,32 @@
 #ifndef MYTIME_H_INCLUDED
 #define MYTIME_H_INCLUDED
 
+//priority Project > Assignment > Revision > Activity
 enum task_type
-{Assignment,Project,Revision,Activity};
-
-typedef struct 
 {
-    int days_since_base;
+    Project,
+    Assignment,
+    Revision,
+    Activity
+};
+
+typedef struct
+{
+    int days_since_base; //the day since the base day
     int time_slot; //if not ,-1
-}Date;
+} Date;
 
-typedef struct 
+typedef struct
 {
-    task_type type;
-    char* id;
-    Date day;
-    int duration;
-}Record;
+    task_type type; // enum type
+    char *id; // subjest code
+    Date day; // struct Date
+    int duration; // number of time slot, need to range
+} Record;
+
+char *start_date; // the period start date "YYYY-MM-DD"
+char *end_date; // the period end date "YYYY-MM-DD"
+int start_time; // the period start time "hh" (0~24)
+int end_time; // the period end time "hh" (0~24)
 
 #endif
--- time.c
+++ time.c
@@ -5,27 +5,4 @@
 
 #include "mytime.h"
 
-enum task_type
-{Assignment,Project,Revision,Activity};
-
-typedef struct 
-{
-    int days_since_base;
-    int time_slot; //if not ,-1
-}Date;
-
-typedef struct 
-{
-    task_type type;
-    char* id;
-    Date day;
-    int duration;
-}Record;
-
-char* start_date;
-char* end_date;
-int start_time;
-int end_time;
-
-
 
