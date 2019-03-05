5f7bd6fabe4dda4b9476047e9f6966bffa9a6317
XU Xiaohchuan <35758677+incandescentxxc@users.noreply.github.com>
2019-03-05 16:57:31 +0800

    Update record.h
    1. fix some grammer mistakes.
    2. fix some mistakes triggered by the sequence of code block.

--- record.h
+++ record.h
@@ -1,13 +1,6 @@
 #ifndef RECORD_H_INCLUDED
 #define RECORD_H_INCLUDED
-
-typedef struct 
-{
-    Record value[SIZE];
-    int count=0;
-    int ptr=0;
-    Data_record* next;
-}Data_record;
+#define SIZE 128
 
 //priority Project > Assignment > Revision > Activity
 enum task_type
@@ -20,15 +13,24 @@ enum task_type
 
 typedef struct
 {
-    task_type type; // enum type
-    char *id;       // subjest code
+    enum task_type type; // enum type
+    char *id;       // subject code
     Date day;       // struct Date
     int duration;   // number of time slot, need to range
 } Record;
 
+typedef struct
+{
+    Record value[SIZE];
+    int count;  //the number of record  in this block
+    int ptr;    // the next available record position in record array
+    struct Data_record* next;
+}Data_record;
 
-void add_data(Data_record* block, Record record)
 
-Record next(Data_record* block)
+
+void add_data(Data_record* block, Record record);
+
+Record next(Data_record* block);
 
 #endif
