e8326a6c6baffd69c622ba178a4bf8b42d493bba
HANSAMA <31698359+hanshanglin@users.noreply.github.com>
2019-04-11 03:32:57 +0800

    Delete DDL.h

--- DDL.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMP2432_PROJECT_DDL_H
-#define COMP2432_PROJECT_DDL_H
-
-#include "record.h"
-#include "mytime.h"
-#include "log.h"
-
-Record** DDL(Data_record* input);
-#endif //COMP2432_PROJECT_DDL_H
